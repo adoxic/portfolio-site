4e74bd2f484e0c037e20536135a0da1f645200df
adoxic <griffinmaeve@gmail.com>
2020-02-19 12:38:10 -0800

    got rid of uneeded skew

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './header/Header';
 import styles from '../reset.css';
+import Cards from './cards/Cards';
 
 const App = () => {
   return (
     <div className={styles}>
       <Header />
+      {/* <Cards /> */}
     </div>
   );
 };
